b3d501f112212996c908b80742acca506817a0d0
Goo Gunmo <ticonhi@naver.com>
2021-05-03 22:30:17 +0900

    <refactor> run_mrc -> reader class
    run_mrc 함수가 하나의 Reader 모델처럼 동작한다고 생각하여 클래스 구조로 재작성하였습니다.
    
    - run_mrc -> Reader class
    - run.py, 그에 맞게 수정
    - reader 폴더 생성

--- prepare.py
+++ prepare.py
@@ -1,18 +1,15 @@
 import os.path as p
 
-from tokenization_kobert import KoBertTokenizer
-from utils_qa import postprocess_qa_predictions
-from datasets import load_from_disk, load_dataset, load_metric
-from transformers import EvalPrediction
-from transformers import AutoConfig, AutoModelForQuestionAnswering, AutoTokenizer
+from reader import DPRReader
 from retrieval.sparse import SparseRetrieval
+from tokenization_kobert import KoBertTokenizer
+from datasets import load_from_disk, load_dataset, load_metric
+from transformers import AutoConfig, AutoModelForQuestionAnswering, AutoTokenizer
 
 
 metric = load_metric("squad")
 
-
-def compute_metrics(p):
-    return metric.compute(predictions=p.predictions, references=p.label_ids)
+READER = {"DPR": DPRReader}
 
 
 def get_retriever(args):
@@ -38,10 +35,11 @@ def get_retriever(args):
         mecab = Mecab()
         retriever = SparseRetrieval(args, tokenize_fn=mecab.morphs)
         retriever.get_sparse_embedding()
+
     return retriever
 
 
-def get_reader_model(args):
+def get_reader_model(args, datasets):
     """
     Get pretrained MRC-Reader model and tokenizer.
     If model setting is KoBERT, then load tokenizer from public KoBERT Tokenizer.
@@ -72,7 +70,9 @@ def get_reader_model(args):
         args.model.model_name_or_path, from_tf=bool(".ckpt" in args.model.model_name_or_path), config=config
     )
 
-    return model, tokenizer
+    reader = READER[args.model.reader_name](args, model, tokenizer, datasets)
+
+    return reader
 
 
 def prepare_dataset(args, is_train=True):
@@ -104,149 +104,3 @@ def prepare_dataset(args, is_train=True):
         datasets["train"] = datasets["train"].select(range(100))
 
     return datasets
-
-
-def preprocess_dataset(args, datasets, tokenizer, is_train=True):
-    """
-    Setup dataset for training/validation/inference.
-    Inner functions
-        - prepare_train_features : for train dataset. tokenizing, offset mapping + answer position that model predicts.
-        - prepare_validation_feature : for validation dataset.  tokenizing, offset mapping.
-
-    :param args
-        - data.max_seq_length
-        - data.doc_stride
-        - data.pad_to_max_length
-        - max_answer_length
-    :param datasets
-    :param tokenizer
-    :param is_train : [True, False]
-        use prepare_train_features function if True. else, use prepare_validation_features function.
-    :return: (preprocessed dataset, postprocessing function for prediction)
-    """
-    data_type = "train" if is_train else "validation"
-
-    column_names = datasets[data_type].column_names
-
-    question_column_name = "question" if "question" in column_names else column_names[0]
-    context_column_name = "context" if "context" in column_names else column_names[1]
-    answer_column_name = "answers" if "answers" in column_names else column_names[2]
-
-    pad_on_right = tokenizer.padding_side == "right"
-    max_seq_length = min(args.data.max_seq_length, tokenizer.model_max_length)
-
-    def prepare_train_features(examples):
-        tokenized_examples = tokenizer(
-            examples[question_column_name if pad_on_right else context_column_name],
-            examples[context_column_name if pad_on_right else question_column_name],
-            truncation="only_second" if pad_on_right else "only_first",
-            max_length=max_seq_length,
-            stride=args.data.doc_stride,
-            return_overflowing_tokens=True,
-            return_offsets_mapping=True,
-            padding="max_length" if args.data.pad_to_max_length else False,
-        )
-
-        sample_mapping = tokenized_examples.pop("overflow_to_sample_mapping")
-        offset_mapping = tokenized_examples.pop("offset_mapping")
-
-        tokenized_examples["start_positions"] = []
-        tokenized_examples["end_positions"] = []
-
-        for i, offsets in enumerate(offset_mapping):
-            input_ids = tokenized_examples["input_ids"][i]
-            cls_index = input_ids.index(tokenizer.cls_token_id)
-
-            sequence_ids = tokenized_examples.sequence_ids(i)
-
-            sample_index = sample_mapping[i]
-            answers = examples[answer_column_name][sample_index]
-
-            if len(answers["answer_start"]) == 0:
-                tokenized_examples["start_positions"].append(cls_index)
-                tokenized_examples["end_positions"].append(cls_index)
-            else:
-                start_char = answers["answer_start"][0]
-                end_char = start_char + len(answers["text"][0])
-
-                token_start_index = 0
-                while sequence_ids[token_start_index] != (1 if pad_on_right else 0):
-                    token_start_index += 1
-
-                token_end_index = len(input_ids) - 1
-                while sequence_ids[token_end_index] != (1 if pad_on_right else 0):
-                    token_end_index -= 1
-
-                if not (offsets[token_start_index][0] <= start_char and offsets[token_end_index][1] >= end_char):
-                    tokenized_examples["start_positions"].append(cls_index)
-                    tokenized_examples["end_positions"].append(cls_index)
-                else:
-                    while token_start_index < len(offsets) and offsets[token_start_index][0] <= start_char:
-                        token_start_index += 1
-                    tokenized_examples["start_positions"].append(token_start_index - 1)
-                    while offsets[token_end_index][1] >= end_char:
-                        token_end_index -= 1
-                    tokenized_examples["end_positions"].append(token_end_index + 1)
-
-        return tokenized_examples
-
-    def prepare_validation_features(examples):
-        tokenized_examples = tokenizer(
-            examples[question_column_name if pad_on_right else context_column_name],
-            examples[context_column_name if pad_on_right else question_column_name],
-            truncation="only_second" if pad_on_right else "only_first",
-            max_length=max_seq_length,
-            stride=args.data.doc_stride,
-            return_overflowing_tokens=True,
-            return_offsets_mapping=True,
-            padding="max_length" if args.data.pad_to_max_length else False,
-        )
-
-        sample_mapping = tokenized_examples.pop("overflow_to_sample_mapping")
-
-        tokenized_examples["example_id"] = []
-
-        for i in range(len(tokenized_examples["input_ids"])):
-            sequence_ids = tokenized_examples.sequence_ids(i)
-            context_index = 1 if pad_on_right else 0
-
-            sample_index = sample_mapping[i]
-            tokenized_examples["example_id"].append(examples["id"][sample_index])
-
-            tokenized_examples["offset_mapping"][i] = [
-                (o if sequence_ids[k] == context_index else None)
-                for k, o in enumerate(tokenized_examples["offset_mapping"][i])
-            ]
-        return tokenized_examples
-
-    def post_processing_function(examples, features, predictions, training_args):
-        predictions = postprocess_qa_predictions(
-            examples=examples,
-            features=features,
-            predictions=predictions,
-            max_answer_length=args.data.max_answer_length,
-            output_dir=training_args.output_dir,
-            prefix="test" if args.train.do_predict else "valid",
-        )
-
-        formatted_predictions = [{"id": k, "prediction_text": v} for k, v in predictions.items()]
-        if training_args.do_predict:
-            return formatted_predictions
-
-        elif training_args.do_eval:
-            references = [{"id": ex["id"], "answers": ex[answer_column_name]} for ex in datasets["validation"]]
-            return EvalPrediction(predictions=formatted_predictions, label_ids=references)
-
-    dataset = datasets[data_type]
-    prepare_func = prepare_train_features if is_train else prepare_validation_features
-
-    dataset = dataset.map(
-        prepare_func,
-        batched=True,
-        batch_size=args.data.batch_size,
-        num_proc=args.data.preprocessing_num_workers,
-        remove_columns=column_names,
-        load_from_cache_file=not args.data.overwrite_cache,
-    )
-
-    return dataset, post_processing_function
