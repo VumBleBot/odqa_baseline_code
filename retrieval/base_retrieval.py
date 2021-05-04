53e68112f612eef502e792a08059d29184b79c2f
ebbunnim <ebbunnim@gmail.com>
2021-05-04 22:26:21 +0900

    <refactor> modify retrieve()

--- retrieval/base_retrieval.py
+++ retrieval/base_retrieval.py
@@ -28,53 +28,50 @@ class Retrieval:
     def get_relevant_doc_bulk(self, queries, k=1):
         raise NotImplementedError
 
-    #  def retrieve_pipeline(self, args, query_or_dataset, topk=1):
-    #      df = self.retrieve(query_or_dataset, topk=topk)
-    #
-    #      if args.train.do_predict is True:
-    #          f = Features(
-    #              {
-    #                  "context": Value(dtype="string", id=None),
-    #                  "id": Value(dtype="string", id=None),
-    #                  "question": Value(dtype="string", id=None),
-    #              }
-    #          )
-    #      else:
-    #          f = Features(
-    #              {
-    #                  "answers": Sequence(
-    #                      feature={"text": Value(dtype="string", id=None), "answer_start": Value(dtype="int32", id=None)},
-    #                      length=-1,
-    #                      id=None,
-    #                  ),
-    #                  "context": Value(dtype="string", id=None),
-    #                  "id": Value(dtype="string", id=None),
-    #                  "question": Value(dtype="string", id=None),
-    #              }
-    #          )
-    #
-    #      datasets = DatasetDict({"validation": Dataset.from_pandas(df, features=f)})
-    #      return datasets
-    #
-    #  def retrieve(self, query_or_dataset, topk=1):  # buld query만을 가정 (query_or_dataset, Dataset, topk)
-    #      assert (
-    #          self.p_embedding is not None
-    #      ), "You must build faiss by self.get_sparse_embedding() before you run self.retrieve()."  # (Question)
-    #
-    #      total = []
-    #      doc_scores, doc_indices = self.get_relevant_doc_bulk(query_or_dataset["question"], k=1)
-    #
-    #      for idx, example in enumerate(tqdm(query_or_dataset, desc="Sparse retrieval: ")):
-    #          tmp = {
-    #              "question": example["question"],
-    #              "id": example["id"],
-    #              "context_id": doc_indices[idx][0],  # retrieved id
-    #              "context": self.contexts[doc_indices[idx][0]],  # retrieved doument
-    #          }
-    #          if "context" in example.keys() and "answers" in example.keys():
-    #              tmp["original_context"] = example["context"]  # original document
-    #              tmp["answers"] = example["answers"]  # original answer
-    #          total.append(tmp)
-    #
-    #      cqas = pd.DataFrame(total)
-    #      return cqas
+    def retrieve(self, query_or_dataset, topk=1):  
+        assert (
+            self.p_embedding is not None
+        ), "get_embedding()을 먼저 수행한 후에 retrieve()를 작동시켜 주세요. "
+
+        total = []
+        doc_scores, doc_indices = self.get_relevant_doc_bulk(query_or_dataset["question"], k=topk)
+
+        for idx, example in enumerate(tqdm(query_or_dataset, desc="Retrieval: ")):
+            for doc_id in range(topk):
+                tmp = {
+                    "question": example["question"],
+                    "id": example["id"],
+                    "context_id": doc_indices[idx][doc_id],  # retrieved id
+                    "context": self.contexts[doc_indices[idx][doc_id]],  # retrieved doument
+                }
+                if "context" in example.keys() and "answers" in example.keys():
+                    tmp["original_context"] = example["context"]  # original document
+                    tmp["answers"] = example["answers"]  # original answer
+                total.append(tmp)
+
+        df = pd.DataFrame(total)
+
+        if args.train.do_predict is True:
+            f = Features(
+                {
+                    "context": Value(dtype="string", id=None),
+                    "id": Value(dtype="string", id=None),
+                    "question": Value(dtype="string", id=None),
+                }
+            )
+        else:
+            f = Features(
+                {
+                    "answers": Sequence(
+                        feature={"text": Value(dtype="string", id=None), "answer_start": Value(dtype="int32", id=None)},
+                        length=-1,
+                        id=None,
+                    ),
+                    "context": Value(dtype="string", id=None),
+                    "id": Value(dtype="string", id=None),
+                    "question": Value(dtype="string", id=None),
+                }
+            )
+
+        datasets = DatasetDict({"validation": Dataset.from_pandas(df, features=f)})
+        return datasets
