ccc8ded8660ec1601193758240cec003501fe4bf
sooyounlee <lee.sooyoun616@gmail.com>
2021-05-05 00:28:20 +0900

    <feat>: BM25 update

--- retrieval/sparse/bm25.py
+++ retrieval/sparse/bm25.py
@@ -1,96 +1,56 @@
-from tqdm.auto import tqdm
-import pandas as pd
 import numpy as np
-
-from datasets import Dataset
 from retrieval.sparse import SparseRetrieval
-
+from scipy import sparse
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 class BM25(SparseRetrieval):
-    def __init__(self, args, tokenize_fn, context_path="wikipedia_documents.json", b=0.75, k1=1.6):
+    def __init__(self, args, tokenize_fn, context_path="wikipedia_documents.json", b=0.75, k1=1.2):
         super().__init__(args, tokenize_fn=tokenize_fn, context_path=context_path)
         self.name = "BM25"
         self.b = b
         self.k1 = k1
+        self.tfidfv = TfidfVectorizer(tokenizer=tokenize_fn, ngram_range=(1, 2), max_features=50000)
         self.avdl = None
 
     def get_sparse_embedding(self):
         super().get_sparse_embedding()
+        self.tfidfv.fit(self.contexts)
+        self.p_embedding = self.tfidfv.transform(self.contexts)
         self.avdl = self.p_embedding.sum(1).mean()
 
-    def retrieve(self, query_or_dataset, topk=1):
-        assert (
-            self.p_embedding is not None
-        ), "You must build faiss by self.get_sparse_embedding() before you run self.retrieve()."
-
-        if isinstance(query_or_dataset, str):
-            doc_scores = self.bm25.get_scores(query_or_dataset)
-            topk_docs = self.bm25.get_top_n(query_or_dataset, self.context, topk)
-            return doc_scores, topk_docs
-
-        elif isinstance(query_or_dataset, Dataset):
-            doc_scores = self.bm25.get_batch_scores(query_or_dataset, self.ids)
-            total = []
-            doc_scores, doc_indices = self.get_relevant_doc_bulk(query_or_dataset["question"], k=1)
-
-            for idx, example in enumerate(tqdm(query_or_dataset, desc="Sparse retrieval: ")):
-                tmp = {
-                    "question": example["question"],
-                    "id": example["id"],
-                    "context_id": doc_indices[idx][0],  # retrieved id
-                    "context": self.contexts[doc_indices[idx][0]],  # retrieved doument
-                }
-                if "context" in example.keys() and "answers" in example.keys():
-                    tmp["original_context"] = example["context"]  # original document
-                    tmp["answers"] = example["answers"]  # original answer
-                total.append(tmp)
-
-            cqas = pd.DataFrame(total)
-            return cqas
-
     def get_relevant_doc(self, query, k=1):
-
         query_vec = self.tfidfv.transform([query])
         assert np.sum(query_vec) != 0, "오류가 발생했습니다. 이 오류는 보통 query에 vectorizer의 vocab에 없는 단어만 존재하는 경우 발생합니다."
 
         b, k1, avdl = self.b, self.k1, self.avdl
 
-        len_context = self.p_embedding.sum(1).A1
+        len_p = self.p_embedding.sum(1).A1
 
-        X = self.p_embedding.tocsc()[:, query_vec.indices]
-        denom = X + (k1 * (1 - b + b * len_context/ avdl))[:, None]
-        idf = self.tfidf._tfidf.idf_[None, query_vec.indices] - 1.
-        numer = X.multiply(np.broadcast_to(idf, X.shape)) * (k1 + 1)
+        p_emb_for_q = self.p_embedding.tocsc()[:, query_vec.indices]
+        denom = p_emb_for_q + (k1 * (1 - b + b * len_p / avdl))[:, None]
+
+        # idf(t) = log [ n / df(t) ] + 1 in sklearn, so it need to be converted
+        # to idf(t) = log [ n / df(t) ] with minus 1
+        idf = self.tfidfv._tfidf.idf_[None, query_vec.indices] - 1.
+        numer = p_emb_for_q.multiply(np.broadcast_to(idf, p_emb_for_q.shape)) * (k1 + 1)
 
         result = (numer / denom).sum(1).A1
 
         if not isinstance(result, np.ndarray):
             result = result.toarray()
 
-        sorted_result_idx = np.argsort(result.squeeze())[::-1]  # 내적값 내림차순 정렬한 인덱스 (test상 2269번 docs가 가장 내적값이 높음)
-        return result.squeeze()[sorted_result_idx].tolist()[:k], sorted_result_idx.tolist()[:k]
+        sorted_result_idx = np.argsort(result)[::-1]
+        return result[sorted_result_idx].tolist()[:k], sorted_result_idx.tolist()[:k]
 
     def get_relevant_doc_bulk(self, queries, k=1):
-        query_vec = self.tfidfv.transform(queries)
-        assert np.sum(query_vec) != 0, "오류가 발생했습니다. 이 오류는 보통 query에 vectorizer의 vocab에 없는 단어만 존재하는 경우 발생합니다."
-
-        b, k1, avdl = self.b, self.k1, self.avdl
-
-        len_context = self.p_embedding.sum(1).A1
-
-        X = self.p_embedding.tocsc()[:, query_vec.indices]
-        denom = X + (k1 * (1 - b + b * len_context/ avdl))[:, None]
-        idf = self.tfidf._tfidf.idf_[None, query_vec.indices] - 1.
-        numer = X.multiply(np.broadcast_to(idf, X.shape)) * (k1 + 1)
-
-        result = (numer / denom).sum(1).A1
-
+        q = super(TfidfVectorizer, self.tfidfv).transform(queries)
+        assert np.sum(q) != 0, "오류가 발생했습니다. 이 오류는 보통 query에 vectorizer의 vocab에 없는 단어만 존재하는 경우 발생합니다."
         doc_scores = []
         doc_indices = []
 
-        for i in range(result.shape[0]):
-            sorted_result = np.argsort(result[i, :])[::-1]
-            doc_scores.append(result[i, :][sorted_result].tolist()[:k])  # 스코어 리스트
-            doc_indices.append(sorted_result.tolist()[:k])  # 예측 index 리스트
+        for query in queries:
+            doc_score, doc_indice = self.get_relevant_doc(query, k)
+            doc_scores.append(doc_score)
+            doc_indices.append(doc_indice)
 
         return doc_scores, doc_indices
