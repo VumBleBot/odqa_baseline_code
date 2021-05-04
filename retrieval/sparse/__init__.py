81f7de2185d317dc2b0bd4c742eb7c95ca5f1168
sooyounlee <lee.sooyoun616@gmail.com>
2021-05-05 00:45:42 +0900

    <feat>: BM25 edit

--- retrieval/sparse/__init__.py
+++ retrieval/sparse/__init__.py
@@ -1,2 +1,3 @@
 from .sparse_base import SparseRetrieval
 from .tfidf import TfidfRetrieval
+from .bm25 import BM25Retrieval
--- retrieval/sparse/bm25.py
+++ retrieval/sparse/bm25.py
@@ -1,25 +1,26 @@
 import numpy as np
-from retrieval.sparse import SparseRetrieval
-from scipy import sparse
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-class BM25(SparseRetrieval):
-    def __init__(self, args, tokenize_fn, context_path="wikipedia_documents.json", b=0.75, k1=1.2):
-        super().__init__(args, tokenize_fn=tokenize_fn, context_path=context_path)
-        self.name = "BM25"
+from retrieval.sparse import SparseRetrieval
+
+class BM25Retrieval(SparseRetrieval):
+    def __init__(self, args, b=0.75, k1=1.2):
+        super().__init__(args)
+
+        mecab = Mecab()
         self.b = b
         self.k1 = k1
-        self.tfidfv = TfidfVectorizer(tokenizer=tokenize_fn, ngram_range=(1, 2), max_features=50000)
+        self.encoder = TfidfVectorizer(tokenizer=mecab.morphs, ngram_range=(1, 2), max_features=50000)
         self.avdl = None
+        self.p_embedding = None
 
-    def get_sparse_embedding(self):
-        super().get_sparse_embedding()
-        self.tfidfv.fit(self.contexts)
-        self.p_embedding = self.tfidfv.transform(self.contexts)
+    def _exec_embedding(self):
+        self.encoder.fit(self.contexts)
+        self.p_embedding = self.encoder.transform(self.contexts)
         self.avdl = self.p_embedding.sum(1).mean()
 
     def get_relevant_doc(self, query, k=1):
-        query_vec = self.tfidfv.transform([query])
+        query_vec = self.encoder.transform([query])
         assert np.sum(query_vec) != 0, "오류가 발생했습니다. 이 오류는 보통 query에 vectorizer의 vocab에 없는 단어만 존재하는 경우 발생합니다."
 
         b, k1, avdl = self.b, self.k1, self.avdl
@@ -31,7 +32,7 @@ class BM25(SparseRetrieval):
 
         # idf(t) = log [ n / df(t) ] + 1 in sklearn, so it need to be converted
         # to idf(t) = log [ n / df(t) ] with minus 1
-        idf = self.tfidfv._tfidf.idf_[None, query_vec.indices] - 1.
+        idf = self.encoder._tfidf.idf_[None, query_vec.indices] - 1.
         numer = p_emb_for_q.multiply(np.broadcast_to(idf, p_emb_for_q.shape)) * (k1 + 1)
 
         result = (numer / denom).sum(1).A1
@@ -43,8 +44,6 @@ class BM25(SparseRetrieval):
         return result[sorted_result_idx].tolist()[:k], sorted_result_idx.tolist()[:k]
 
     def get_relevant_doc_bulk(self, queries, k=1):
-        q = super(TfidfVectorizer, self.tfidfv).transform(queries)
-        assert np.sum(q) != 0, "오류가 발생했습니다. 이 오류는 보통 query에 vectorizer의 vocab에 없는 단어만 존재하는 경우 발생합니다."
         doc_scores = []
         doc_indices = []
 
