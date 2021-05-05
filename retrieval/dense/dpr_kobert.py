08316492270dce20585f394003392d0343e6c6e5
Goo Gunmo <ticonhi@naver.com>
2021-05-05 17:10:01 +0900

    <fix> AutoTokenizer -> KoBertTokenizer

--- retrieval/dense/dpr_kobert.py
+++ retrieval/dense/dpr_kobert.py
@@ -1,9 +1,9 @@
-from transformers import BertTokenizer
 from retrieval.dense import DprRetrieval
+from tokenization_kobert import KoBertTokenizer
 
 
 class DprKobertRetrieval(DprRetrieval):
     def __init__(self, args):
         super().__init__(args)
         self.backbone = "monologg/kobert"
-        self.tokenizer = BertTokenizer.from_pretrained(self.backbone)
+        self.tokenizer = KoBertTokenizer.from_pretrained(self.backbone)
