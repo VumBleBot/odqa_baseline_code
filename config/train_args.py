a41b44f03a68b096e9a4dd289132dcf27cf57caa
SeongIkKim <kpic1638@gmail.com>
2021-05-17 07:14:19 +0900

    <docs> add do_ensemble

--- config/train_args.py
+++ config/train_args.py
@@ -10,4 +10,5 @@ class TrainArguments:
 
     output_dir: Optional[str] = field(default="train_checkpoint", metadata={"help": "checkpoint_dir"})
     pororo_prediction: Optional[bool] = field(default=False, metadata={"help": "pororo mrc model voting(ensemble)"})
+    do_ensemble: Optional[bool] = field(default=False, metadata={"help": "use ensemble.py for prediction"})
     # TrainArguments는 config/ST00.json 에서 수정해 주시면 됩니다.
