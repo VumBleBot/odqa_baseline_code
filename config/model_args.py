4fa0c1eb3338d7d47388e185b4a406f600516cfd
Goo Gunmo <ticonhi@naver.com>
2021-05-30 22:09:22 +0900

    <fix> dense_train_dataset change default

--- config/model_args.py
+++ config/model_args.py
@@ -12,10 +12,7 @@ class ModelArguments:
         default="bert-base-multilingual-cased",
         metadata={"help": "Reader Backbone, Path to pretrained model or model identifier from huggingface.co/models"},
     )
-    model_path: str = field(
-        default="",
-        metadata={"help": "checkpoint path to load(used when evaluate&test)"},
-    )
+    model_path: str = field(default="", metadata={"help": "checkpoint path to load(used when evaluate&test)"})
 
     retriever_name: str = field(default="TFIDF", metadata={"help": "this args used in tools/get_retriever"})
     reader_name: str = field(default="DPR")
@@ -25,5 +22,6 @@ class ModelArguments:
     )
 
     tokenizer_name: Optional[str] = field(
-        default="bert-base-multilingual-cased", metadata={"help": "Pretrained tokenizer name or path if not the same as model_name"}
+        default="bert-base-multilingual-cased",
+        metadata={"help": "Pretrained tokenizer name or path if not the same as model_name"},
     )
